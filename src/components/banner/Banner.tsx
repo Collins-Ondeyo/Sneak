a8c1569f6b59bbd40629e63de66c74aef60c1d6d
collins ondeyo <ondeyocollins5@gmail.com>
2024-02-08 09:07:40 +0300

    create the banner components and update the banner to render relevant content when the topbar buttons are clicked

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -4,10 +4,30 @@ import { cn } from "@/utils/cn";
 import clsx from "clsx";
 import { MaxWidthWrapper } from "../UI/MaxWidthWrapper";
 
+import { Brands } from "./bannerComponents/Brands";
+import { Cart } from "./bannerComponents/Cart";
+import { Search } from "./bannerComponents/Search";
+import { UserProfile } from "./bannerComponents/UserProfile";
+
 export const Banner = ({ className }: {
     className: string;
 }) => {
     const bannerStore = useTopBannersStore();
+    const child = () => {
+        switch (bannerStore.activeBanner) {
+            case "BrandsBanner":
+                return <Brands />
+            case "CartBanner":
+                return <Cart />
+            case "SearchBanner":
+                return <Search />
+            case "UserProfileBanner":
+                return <UserProfile />
+            default:
+                console.error("Wrong banner")
+                break;
+        }
+    }
     return (
         <div className={cn("transition-[opacity, transform] duration-200 ease-in-out", className, clsx({
             "opacity-100 visible -translate-y-0": bannerStore.activeBanner != null,
@@ -19,7 +39,7 @@ export const Banner = ({ className }: {
             {/* BANNER CONTENT */}
             <div className="absolute top-0 left-0 h-fit w-full bg-white">
                 <MaxWidthWrapper className="py-2">
-                banner content
+                    {child()}
                 </MaxWidthWrapper>
             </div>
         </div>
---
+++ src/components/banner/bannerComponents/Cart.tsx
@@ -0,0 +1,7 @@
+export const Cart = () => {
+    return (
+        <div>
+            <h1>Shopping Cart</h1>
+        </div>
+    )
+}
